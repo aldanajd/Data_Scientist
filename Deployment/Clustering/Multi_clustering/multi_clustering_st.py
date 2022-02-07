3103182a77324a400f513ebb65da5bf44f1072e1
Jorge Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2022-02-07 10:11:52 -0500

    Update multi_clustering_st.py

--- Deployment/Clustering/Multi_clustering/multi_clustering_st.py
+++ Deployment/Clustering/Multi_clustering/multi_clustering_st.py
@@ -2,23 +2,26 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import joblib
 
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import scale
 
 sns.set()
 
-data = pd.read_csv('Deployment/Clustering/Multi_clustering/loyalty_satisfaction.csv')
+data = joblib.load('Deployment/Clustering/Multi_clustering/loyalty_satisfaction_clusters.pkl')
 df = data.copy()
-df_scaled = pd.DataFrame(scale(df), columns=df.columns)
 
 clusters = st.sidebar.select_slider('Select Number of Clusters',[2,3,4,5], key=2)
 
-import joblib
+fig, ax = plt.subplots(2,1, figsize=[7,10])
+ax[0].scatter(df.iloc[:,0],df.iloc[:,1])
+ax[0].title.set_text('Plot without Clusters')
+ax[0].set_ylabel(df.columns[1])
 
-kmeans = kmeans_scaled = KMeans(clusters)
-kmeans_scaled.fit(df_scaled)
+ax[1].scatter(df.iloc[:,0],df.iloc[:,1], c=df[str(clusters)+'_Clusters'], cmap='cool')
+ax[1].title.set_text('Plot with Clusters')
+ax[1].set_xlabel(df.columns[0])
+ax[1].set_ylabel(df.columns[1])
 
-for i in [2,3,4,5]:
-    joblib.dump(KMeans(i).fit(df),str(i)+'_kmeans.pkl')
-    joblib.dump(KMeans(i).fit(df_scaled),str(i)+'_kmeans_scaled.pkl')
+st.pyplot(fig)
