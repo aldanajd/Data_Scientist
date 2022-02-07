e25407c4aac8e5fb5be61d698e5d4eddf125c37f
Jorge Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2022-02-07 00:11:42 -0500

    Update and rename clustering_st.py to multi_clustering_st.py

--- Deployment/Clustering/Multi_clustering/multi_clustering_st.py
+++ Deployment/Clustering/Multi_clustering/multi_clustering_st.py
@@ -119,3 +119,31 @@ if data == 'Insert your cleaned data.csv':
 
         else:
             st.write('Select Features')
+            
+else:
+
+    data = pd.read_csv'Deployment/Clustering/Multi_clustering/loyalty_satisfaction.csv')
+    df = data.copy()
+    df_scaled = pd.DataFrame(scale(df), columns=df.columns)
+
+    clusters = st.sidebar.select_slider('Select Number of Clusters',[2,3,4,5], key=2)
+
+    fig, ax = plt.subplots(2,1, figsize=[7,10])
+    ax[0].scatter(df.iloc[:,0],df.iloc[:,1])
+    ax[0].title.set_text('Plot without Clusters')
+    ax[0].set_ylabel(df.columns[1])
+
+    kmeans = kmeans_scaled = KMeans(clusters)
+    kmeans.fit(df)
+    kmeans_scaled.fit(df_scaled)
+
+    df[str(clusters)+'_Clusters'] = kmeans.fit_predict(df)
+    df_scaled[str(clusters)+'_Clusters'] = kmeans.fit_predict(df_scaled)
+
+    ax[1].scatter(df.iloc[:,0],df.iloc[:,1], c=df_scaled[str(clusters)+'_Clusters'], cmap='cool')
+    ax[1].title.set_text('Plot with Clusters')
+    ax[1].set_xlabel(df.columns[0])
+    ax[1].set_ylabel(df.columns[1])
+
+    st.pyplot(fig)
+            
