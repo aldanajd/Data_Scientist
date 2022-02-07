33e21ab4051e94d405e03879adf8ce64ba752924
Jorge Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2022-02-07 09:55:08 -0500

    Update multi_clustering_st.py

--- Deployment/Clustering/Multi_clustering/multi_clustering_st.py
+++ Deployment/Clustering/Multi_clustering/multi_clustering_st.py
@@ -14,33 +14,26 @@ df_scaled = pd.DataFrame(scale(df), columns=df.columns)
 
 clusters = st.sidebar.select_slider('Select Number of Clusters',[2,3,4,5], key=2)
 
-@st.cache  
-def kmean(df,df_scaled,clusters):
+import joblib
 
-    kmeans = kmeans_scaled = KMeans(clusters)
-    kmeans.fit(df)
-    kmeans_scaled.fit(df_scaled)
+kmeans = kmeans_scaled = KMeans(clusters)
+kmeans_scaled.fit(df_scaled)
 
-    df[str(clusters)+'_Clusters'] = kmeans.fit_predict(df)
-    df_scaled[str(clusters)+'_Clusters'] = kmeans.fit_predict(df_scaled)
+for i in [2,3,4,5]:
+    joblib.dump(KMeans(i).fit(df),i+'_kmeans.pkl')
+    joblib.dump(KMeans(i).fit(df_scaled),i+'_kmeans_scaled.pkl')
 
-kmeans = kmean(df,df_scaled,clusters)
-kmeans
+df[str(clusters)+'_Clusters'] = kmeans.fit_predict(df)
+df_scaled[str(clusters)+'_Clusters'] = kmeans.fit_predict(df_scaled)
 
-def plots(df, df_scaled, clusters):
+fig, ax = plt.subplots(2,1, figsize=[7,10])
+ax[0].scatter(df.iloc[:,0],df.iloc[:,1])
+ax[0].title.set_text('Plot without Clusters')
+ax[0].set_ylabel(df.columns[1])
 
-    fig, ax = plt.subplots(2,1, figsize=[7,10])
-    ax[0].scatter(df.iloc[:,0],df.iloc[:,1])
-    ax[0].title.set_text('Plot without Clusters')
-    ax[0].set_ylabel(df.columns[1])
+ax[1].scatter(df.iloc[:,0],df.iloc[:,1], c=df_scaled[str(clusters)+'_Clusters'], cmap='cool')
+ax[1].title.set_text('Plot with Clusters')
+ax[1].set_xlabel(df.columns[0])
+ax[1].set_ylabel(df.columns[1])       
 
-    ax[1].scatter(df.iloc[:,0],df.iloc[:,1], c=df_scaled[str(clusters)+'_Clusters'], cmap='cool')
-    ax[1].title.set_text('Plot with Clusters')
-    ax[1].set_xlabel(df.columns[0])
-    ax[1].set_ylabel(df.columns[1])       
-
-    return fig
-
-subplot = plots(df, df_scaled, clusters)
-
-st.pyplot(subplot)
+st.pyplot(dig)
