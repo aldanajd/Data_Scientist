6697f71a49e0cbf24304897eb2ddf3e985f52f53
Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2022-04-05 21:46:24 -0500

    Update img_rest_st.py

--- Deployment/Media_Restoration/Image_Restoration/img_rest_st.py
+++ Deployment/Media_Restoration/Image_Restoration/img_rest_st.py
@@ -2,11 +2,13 @@ import streamlit as st
 import os 
 
 upload = st.sidebar.file_uploader("Upload an image...", type=["jpg", "png"])
-convert = 'pip install realesrgan'
+convert = 'git clone https://github.com/xinntao/Real-ESRGAN.git'
+convert2 = 'pip install realesrgan'
 
 if upload != None:
-
+    
     os.system(convert)
+    os.system(convert2)
 
     st.image(upload, caption='Original Image', width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     st.image(upload, caption='Upscale Image', width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
