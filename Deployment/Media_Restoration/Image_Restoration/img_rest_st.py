888aca4656a0b507c3e4299199838faeacd96315
Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2022-04-05 22:23:36 -0500

    Update img_rest_st.py

--- Deployment/Media_Restoration/Image_Restoration/img_rest_st.py
+++ Deployment/Media_Restoration/Image_Restoration/img_rest_st.py
@@ -5,13 +5,11 @@ import platform
 upload = st.sidebar.file_uploader("Upload an image...", type=["jpg", "png"])
 convert = 'git clone https://github.com/xinntao/Real-ESRGAN.git'
 convert2 = 'pip install realesrgan'
-asd = os.name
-fgh = platform.system()
 
 if upload != None:
     
     os.system(convert)
     os.system(convert2)
 
-    st.image(upload, caption=asd, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-    st.image(upload, caption=fgh, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image(upload, caption='Original Image', width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image(upload, caption='Upscaled Image, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
