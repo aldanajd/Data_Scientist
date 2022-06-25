6e59a370b67f55aa86005af6a034ee4e5f374317
Daniel Aldana <aldanajorgedaniel2@gmail.com>
2022-06-25 13:18:45 -0400

    uploaded sql_python v2.0

---
+++ Machine Learning/Classification/SQL_Python_v2/editables.py
@@ -0,0 +1,14 @@
+import os
+
+# Titanic data set path
+path = os.path.join('C:', os.sep, 'users', 'daniel', 'desktop', 'study notes', '0 - Data Sets', 'titanic.csv')
+
+# Credentials to connect to the database
+credentials = {
+    'dbs'      : '',
+    'host'     : '',
+    'dbname'   : '',
+    'user'     : '',
+    'password' : '',
+    'port'     : '',
+}
